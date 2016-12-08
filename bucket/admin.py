36da9188806ef40cbfc9e9df69c659a38d8d28dc
andela-rwachira <rehema.wachira@andela.com>
2016-12-08 18:04:42 +0300

    Ft-Allow bucketlist interactions via admin dashboard

--- bucket/admin.py
+++ bucket/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Bucketlist, Item
 
-# Register your models here.
+"""
+Allows admins to create/update/delete
+bucketlists and items from the admin dashboard.
+"""
+admin.site.register(Bucketlist)
+admin.site.register(Item)
