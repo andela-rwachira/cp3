63e9fe236c8a53c8f4ee44119f2026ed59044cfa
andela-rwachira <rehema.wachira@andela.com>
2016-12-13 19:23:32 +0300

    Ft-add all api endpoints

--- bucket/serializers.py
+++ bucket/serializers.py
@@ -3,26 +3,11 @@ from django.contrib.auth.models import User
 from .models import Bucketlist, Item
 
 
-class UserSerializer(serializers.ModelSerializer):
-    """
-    Seralizes the User model.
-    """
-    bucketlists = serializers.HyperlinkedRelatedField(many=True,
-                                                      read_only=True,
-                                                      view_name='bucketlists')
-
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'password', 'email', 'bucketlists')
-
-
 class BucketlistSerializer(serializers.ModelSerializer):
     """
     Seralizes the Bucketlist model.
     """
-    items = serializers.HyperlinkedRelatedField(many=True,
-                                                read_only=True,
-                                                view_name='items')
+    items = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = Bucketlist
--- bucket/views.py
+++ bucket/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
+
+from .models import Bucketlist, Item
+from .serializers import BucketlistSerializer, ItemSerializer
+
+
+class BucketlistAPIView(generics.ListCreateAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+    permission_classes = (IsAdminUser,)
+
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
+
+
+class BucketlistDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
+
+
+class ItemAPIView(generics.CreateAPIView):
+    serializer_class = ItemSerializer
+
+    def perform_create(self, serializer):
+        queryset = Bucketlist.objects.all()
+        bucket_id = self.kwargs.get('bucket')
+        bucketlist = queryset.get(id=bucket_id)
+        serializer.save(created_by=self.request.user,
+                        bucket=bucketlist)
+
+
+class ItemDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        queryset = Item.objects.all()
+        bucket = self.kwargs.get('bucket')
+        item = self.kwargs.get('pk')
+        return queryset.filter(bucket=bucket, id=item)
