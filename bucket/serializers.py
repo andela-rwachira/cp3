599ac975f25b3b5cfea75874b8f9c26293db349e
andela-rwachira <rehema.wachira@andela.com>
2016-12-14 14:02:06 +0300

    Ft-Add serializer methods to return user and bucket names instead of id nums

--- bucket/serializers.py
+++ bucket/serializers.py
@@ -7,18 +7,34 @@ class BucketlistSerializer(serializers.ModelSerializer):
     Seralizes the Bucketlist model.
     """
     items = serializers.StringRelatedField(many=True)
+    created_by = serializers.SerializerMethodField()
 
     class Meta:
         model = Bucketlist
         fields = ('id', 'name', 'date_created',
                   'date_modified', 'created_by', 'items')
 
+    def get_created_by(self, obj):
+        """Returns user name instead of id number."""
+        return str(obj.created_by.username)
+
 
 class ItemSerializer(serializers.ModelSerializer):
     """
     Seralizes the Item model.
     """
+    created_by = serializers.SerializerMethodField()
+    bucket = serializers.SerializerMethodField()
+
     class Meta:
         model = Item
         fields = ('id', 'name', 'date_created',
                   'date_modified', 'created_by', 'bucket', 'done')
+
+    def get_created_by(self, obj):
+        """Returns user name instead of id number."""
+        return str(obj.created_by.username)
+
+    def get_bucket(self, obj):
+        """Returns bucketlist name instead of id number."""
+        return str(obj.bucket.name)
