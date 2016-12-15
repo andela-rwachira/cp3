fd1ce1216bfc78380b954d5bcf7bd80bb575256b
andela-rwachira <rehema.wachira@andela.com>
2016-12-15 20:05:21 +0300

    Tests-Add base test case

--- bucket/tests/test_bucketlist.py
+++ bucket/tests/test_bucketlist.py
@@ -1,35 +1,45 @@
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APITestCase
-
 from bucket.models import Bucketlist
+from test_base import BaseTestCase
 
 
-class TestBucketlistAPIView(APITestCase):
+class TestBucketlistAPIView(BaseTestCase):
     """
     Test bucketlist interactions.
     """
-    def setUp(self):
-        self.user = User.objects.create(username='testuser', password='pass')
-        self.token = Token.objects.create(user=self.user)
-        self.api_authentication()
-
-    def api_authentication(self):
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
     def test_create_new_bucketlist(self):
         """Tests a new bucketlist can be added."""
         response = self.client.post('/api/bucket/', {'name': 'bucketlist'},
                                     format='json')
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data, {'detail': 'bucketlist'})
+        self.assertEqual(response.data['name'], 'bucketlist')
+
         # asserts that the bucketlist has been crated in the db
         self.assertTrue(Bucketlist.objects.filter(name='bucketlist'))
+
+    def test_invalid_name(self):
+        """Tests error raised when empty string passed."""
+        response = self.client.post('/api/bucket/', {'name': ''},
+                                    format='json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['name'][0], 'This field may not be blank')
+
+    def test_duplicates_prevented(self):
+        """
+        Tests error is raised for duplicates.
+  
+        A bucket named 'testbucketlist' was already created in setUp.
+        """
+        response = self.client.post('/api/bucket/', {'name': 'testbucketlist'},
+                                    format='json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['non_field_errors'][0], 'This bucketlist already exists')   
 
     def test_return_all_bucketlists(self):
         """Tests retrieval of all bucketlists."""
         response = self.client.get('/api/bucket/', format='json')
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['count'], Bucketlist.objects.count())
 
     def test_get_bucketlist(self):
         """
@@ -37,31 +47,69 @@ class TestBucketlistAPIView(APITestCase):
         Two test bucketlists were added in the setUp to
         test that the id parameter works correctly.
         """
-        response = self.client.get('/api/bucket/1/', format='json')
+        response = self.client.get('/api/bucket/2/', format='json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {'detail': 'testbucketlist'})
+        self.assertEqual(response.data['name'], 'testbucketlist')
+
+    def test_invalid_bucketlist_get_request(self):
+        """Tests error raised for an invalid get request."""
+        response = self.client.get('/api/bucket/3/', format='json')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data['detail'], 'Not found')
 
     def test_update_bucketlist(self):
         """Tests a bucketlist can be updated."""
         response = self.client.put('/api/bucket/1/', {'name': 'updated'},
                                    format='json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {'detail': 'updated'})
+        self.assertEqual(response.data['name'], 'updated')
+
+    def test_invalid_update_request(self):
+        """Tests error raised when requested item doesn't exist."""
+        response = self.client.put('/api/bucket/3/', {'name': 'updated'},
+                                   format='json')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data['detail'], 'Not found')
 
     def test_duplicates_prevented_during_updates(self):
         """
         Tests error is raised when an updated bucketlist creates
         a duplicate.
-        
+
         A bucket named 'testbucketlist' was already created in setUp.
         """
-        response = self.client.put('/api/bucket/1/',
+        response = self.client.put('/api/bucket/2/',
                                    {'name': 'testbucketlist'},
                                    format='json')
         self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['non_field_errors'][0], 'This bucketlist already exists')
 
     def test_delete_bucketlist(self):
         """Tests bucketlist deletion."""
         response = self.client.delete('/api/bucket/1/', format='json')
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Bucketlist.objects.filter(name='testbucketlist'), None)
+
+    def test_invalid_delete(self):
+        """Tests error raised for an invalid delete request."""
+        response = self.client.delete('/api/bucket/3/', format='json')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data['detail'], 'Not found')
+
+    def test_results_limit(self):
+        """Tests specified number of results retrieved from GET request."""
+        response = self.client.get('/bucketlists/?limit=1', format='json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {'detail': 'deleted'})
+        self.assertTrue(response.data['next'])
+
+    def test_pagination(self):
+        """Tests specified number of pages returned from GET request."""
+        response = self.client.get('/bucketlists/?page=1', format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.data['next'], 'null') # or null
+
+    def test_search_by_bucketlist_name(self):
+        """Tests bucketlist can be retrieved from search."""
+        response = self.client.get('/bucketlists/?search=Testbucketlist')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.data['name'], 'testbucketlist')
--- bucket/tests/test_item.py
+++ bucket/tests/test_item.py
@@ -1,22 +1,58 @@
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APITestCase
-
 from bucket.models import Item
+from test_base import BaseTestCase
 
 
-class TestItemAPIView(APITestCase):
+class TestItemAPIView(BaseTestCase):
     """
     Test bucketlist item interactions.
     """
+
     def test_create_new_item(self):
         """Tests a new item can be added."""
         response = self.client.post('/api/bucket/1/items/', {'name': 'item'},
                                     format='json')
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.data, {'detail': 'item'})
+        self.assertEqual(response.data['name'], 'item')
+
         # asserts that the item has been crated in the db
-        self.assertTrue(Item.objects.filter(name='item'))
+        self.assertTrue(Item.objects.filter(name='testitem'))
+
+    def test_done_defaults_to_false(self):
+        """
+        Tests done field defaults to False
+        for new items.
+        """
+        response = self.client.post('/api/bucket/1/items/',
+                                    {'name': 'an item'},
+                                    format='json')
+        self.assertEqual(response.status_code, 200)
+        item = Item.objects.get(name='an item')
+        self.assertTrue(item.done, False)
+
+    def test_bucketlist_validation_for_new_item(self):
+        """
+        Tests error raised if bucketlist doesn't exist
+        before creating a new item.
+        """
+        pass
+
+    def test_invalid_name(self):
+        """Tests error raised when empty string passed."""
+        response = self.client.post('/api/bucket/1/items/', {'name': ''},
+                                    format='json') 
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['name'][0], 'This field may not be blank')
+
+    def test_duplicates_prevented(self):
+        """
+        Tests error is raised for duplicates.
+  
+        An item named 'testitem' was already created in setUp.
+        """
+        response = self.client.post('/api/bucket/1/items/', {'name': 'testitem'},
+                                    format='json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['non_field_errors'][0], 'This item already exists')
 
     def test_get_item(self):
         """
@@ -26,14 +62,51 @@ class TestItemAPIView(APITestCase):
         """
         response = self.client.get('/api/bucket/1/items/2/', format='json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {'detail': 'testitem2'})
+        self.assertEqual(response.data['name'], 'testitem2')
 
-    def test_update_bucketlist(self):
-        """Tests a item can be updated."""
-        response = self.client.put('/api/bucket/1/items/1/', {'name': 'updated'},
+    def test_invalid_item_get_request(self):
+        """Tests error raised for an invalid get request."""
+        response = self.client.get('/api/bucket/1/items/3', format='json')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data['detail'], 'Not found')
+
+    def test_invalid_update_request(self):
+        """Tests error raised when requested item doesn't exist."""
+        response = self.client.post('/bucketlists/1/items/3',
+                                    {'name': 'update'},
+                                    format='json')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data['detail'], 'Not found')
+
+    def test_update_name_field(self):
+        """Tests name field can be updated."""
+        response = self.client.put('/api/bucket/1/items/1/',
+                                   {'name': 'updated'},
                                    format='json')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {'detail': 'updated'})
+        self.assertEqual(response.data['name'], 'updated')
+
+    def test_invalid_name_update(self):
+        """Tests error raised if empty string is passed."""
+        response = self.client.put('/api/bucket/1/items/1/',
+                                  {'name': ''}, format='json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['name'][0], 'This field may not be blank')
+
+    def test_update_done_field(self):
+        """Tests done field can be updated."""
+        response = self.client.patch('/api/bucket/1/items/1/',
+                                     {'done': 'True'},
+                                     format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['done'], True) #or 'true'
+
+    def test_invalid_done_update(self):
+        """Tests error raised if empty string is passed."""
+        response = self.client.patch('/api/bucket/1/items/1/',
+                                     {'done': ''}, format='json')
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['done'][0], '"" is not a valid boolean') # or "\"\" is not a valid boolean."
 
     def test_duplicates_prevented_during_updates(self):
         """
@@ -42,13 +115,20 @@ class TestItemAPIView(APITestCase):
         
         An item named 'testitem' was already created in setUp.
         """
-        response = self.client.put('/api/bucket/1/items/1/',
+        response = self.client.put('/api/bucket/1/items/2/',
                                    {'name': 'testitem'},
                                    format='json')
         self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.data['non_field_errors'][0], 'This item already exists')
 
     def test_delete_bucketlist(self):
         """Tests item deletion."""
         response = self.client.delete('/api/bucket/1/items/1/', format='json')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {'detail': 'deleted'})
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Item.objects.filter(name='testitem'), None)
+
+    def test_invalid_delete(self):
+        """Tests error raised for an invalid delete request."""
+        response = self.client.delete('/api/bucket/1/items/3/', format='json')
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data['detail'], 'Not found')
