4503179ff5008b9bff82c01a3b7e89229d5f3afe
andela-rwachira <rehema.wachira@andela.com>
2016-12-22 19:56:25 +0300

    Ch-amend item serializer to enable angular connection

--- bucket/serializers.py
+++ bucket/serializers.py
@@ -2,32 +2,6 @@ from rest_framework import serializers
 from bucket.models import Bucketlist, Item
 
 
-class BucketlistSerializer(serializers.ModelSerializer):
-    """
-    Seralizes the Bucketlist model.
-    """
-    items = serializers.StringRelatedField(many=True, read_only=True)
-    created_by = serializers.SerializerMethodField()
-
-    class Meta:
-        model = Bucketlist
-        fields = ('id', 'name', 'date_created',
-                  'date_modified', 'created_by', 'items')
-
-    def validate(self, data):
-        """Checks for duplicates."""
-        user = self.context['request'].user
-        name = data['name']
-        queryset = Bucketlist.objects.filter(name=name, created_by=user)
-        if queryset.exists():
-            raise serializers.ValidationError('This bucketlist already exists.')
-        return data
-
-    def get_created_by(self, obj):
-        """Returns user name instead of id number."""
-        return str(obj.created_by.username)
-
-
 class ItemSerializer(serializers.ModelSerializer):
     """
     Seralizes the Item model.
@@ -55,3 +29,30 @@ class ItemSerializer(serializers.ModelSerializer):
     def get_bucket(self, obj):
         """Returns bucketlist name instead of bucketlist id."""
         return str(obj.bucket.name)
+
+
+class BucketlistSerializer(serializers.ModelSerializer):
+    """
+    Seralizes the Bucketlist model.
+    """
+    # items = serializers.StringRelatedField(many=True, read_only=True)
+    items = ItemSerializer(many=True, read_only=True)
+    created_by = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Bucketlist
+        fields = ('id', 'name', 'date_created',
+                  'date_modified', 'created_by', 'items')
+
+    def validate(self, data):
+        """Checks for duplicates."""
+        user = self.context['request'].user
+        name = data['name']
+        queryset = Bucketlist.objects.filter(name=name, created_by=user)
+        if queryset.exists():
+            raise serializers.ValidationError('This bucketlist already exists.')
+        return data
+
+    def get_created_by(self, obj):
+        """Returns user name instead of id number."""
+        return str(obj.created_by.username)
